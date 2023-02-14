609674acf58be213b1a814c2f9f15306d30d206c
Lilibeth Sequeira <Lilibeth.Sequeira@ooqia.com>
2023-02-14 00:02:39 -0600

    feat(message): create addMessage action to send messages

--- src/views/MessagesView.vue
+++ src/views/MessagesView.vue
@@ -12,6 +12,7 @@ const contactStore = useContactsStore()
 const end = ref(null)
 const channelId = ref(null)
 const title = ref('')
+const message = ref('')
 
 const messagesView = computed(() => messagesStore.findMessagesByChannelId(channelId.value).map((message) => {
   const author = contactStore.getContactById(message.author)
@@ -22,6 +23,11 @@ const messagesView = computed(() => messagesStore.findMessagesByChannelId(channe
     self: author.id === 1
   }
 }))
+
+const addMessage = () => {
+  messagesStore.addMessage(channelId.value, message.value);
+  message.value = ''
+}
 
 const scrollToBottom = () => {
   end.value?.scrollIntoView({
@@ -67,12 +73,12 @@ scrollToBottom()
       />
       <span ref="end"></span>
     </div>
-    <footer>
-      <textarea rows="3"></textarea>
+    <form class="footer" @submit.prevent="addMessage">
+      <textarea rows="3" v-model="message"></textarea>
       <button>
         <Icon icon="carbon:send-alt" />
       </button>
-    </footer>
+    </form>
   </div>
 </template>
 
@@ -97,7 +103,7 @@ scrollToBottom()
   .content {
     @apply flex flex-col gap-4 p-4 h-full overflow-y-auto;
   }
-  footer {
+  .footer {
     @apply flex p-2;
     textarea {
       @apply w-full px-2 py-2 resize-none bg-zinc-800 rounded-tl-md rounded-bl-md focus:outline-none;
